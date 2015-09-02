152906027e5d2df931f74b41a67098cf18c53857
Mike Solomon <msolomon@thrillist.com>
2015-09-02 00:46:19 -0400

    Add comments

--- sum_time.rb
+++ sum_time.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'rest-client'
 require 'time'
 
+# Find all issues that are assigned to this user and have a worklog date of today
 url = "#{ENV['JIRA_URL']}/rest/api/2/search?jql=assignee = #{ENV['USERNAME']} and worklogDate = startOfDay()&fields=worklog"
 headers = { 
 	:"Authorization" => "Basic #{ENV['JIRA_CREDENTIALS']}",
@@ -20,8 +21,9 @@ today_start =  Time.new(time_now.year,time_now.month,time_now.day)
 today_end =  today_start + 86399
 
 issues.each do |issue|
+	# If there are more than 20 worklogs for this issue, query the worklogs for this issue only to get them all
+	# This is a limitation of JIRA (see https://ecosystem.atlassian.net/browse/ACJIRA-72)
 	if issue["fields"]["worklog"]["total"] > 20
-		# We need to query this issue separately
 		issue_key = issue["key"]
 		url = "#{ENV['JIRA_URL']}/rest/api/2/issue/#{issue_key}/worklog"
 		this_issue_worklogs = JSON.parse( RestClient.get(url, headers) )
@@ -30,6 +32,7 @@ issues.each do |issue|
 		worklogs = issue["fields"]["worklog"]["worklogs"]
 	end
 
+	# If the worklog's author is this user and the update time is from today, add the time spent to the total time spent today
 	worklogs.each do |worklog|
 		worklog_update_time = Time.parse(worklog["updated"])
 		if worklog["author"]["key"] == ENV['USERNAME'] && (today_start..today_end).cover?(worklog_update_time)
@@ -38,6 +41,7 @@ issues.each do |issue|
 	end
 end
 
+# Convert the seconds of time spent into hours and minutes
 total_time = Time.at(total_seconds_logged).utc.strftime("%Hh %Mm")
 
 puts total_time
