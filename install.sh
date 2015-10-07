257eeefec9e0f056ff1d0c593378729899f1b7dc
Jacinto Shy <jacinto@thrillist.com>
2015-10-07 15:18:00 -0400

    Make install script safer.

--- install.sh
+++ install.sh
@@ -4,7 +4,24 @@ set -e
 
 echo "Installing timesheet-githook..."
 pushd `git rev-parse --git-dir`/hooks > /dev/null
-git clone https://github.com/msolomonTMG/timesheet-githook.git
+
+if [ -f post-commit ] || [ -L post-commit ]; then
+    echo "A post-commit file already exists."
+    read -p "Overwrite? [y/N] " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        exit 1
+    fi
+    rm -f post-commit
+fi
+
+if [ -d timesheet-githook ]; then
+    pushd timesheet-githook > /dev/null
+    git pull origin
+    popd > /dev/null
+else
+    git clone https://github.com/msolomonTMG/timesheet-githook.git
+fi
 ln -s timesheet-githook/post-commit post-commit
 chmod +x post-commit timesheet-githook/sum_time.rb
 
